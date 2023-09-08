007b859dbd5c6e136c93d4d23de175bf9e34005c
Ahmmed Hashim <ahmedhasim209>
2023-09-08 11:59:56 +0300

    setting box

--- js/master.js
+++ js/master.js
@@ -1,3 +1,11 @@
+let settings = document.querySelector(".settings-box");
+let gear = document.querySelector(".fa-gear");
+
+gear.onclick = function () {
+  this.classList.toggle("fa-spin");
+  settings.classList.toggle("open");
+};
+
 let land = document.querySelector(".landing-page");
 let imgArray = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
 setInterval(function () {
