e6aac38fdadcbfabf1ff57b14eb0c083518787c1
Ahmmed Hashim <ahmedhasim209>
2023-09-22 22:32:55 +0300

    Add Switch Colors feature

--- js/master.js
+++ js/master.js
@@ -6,6 +6,17 @@ gear.onclick = function () {
   settings.classList.toggle("open");
 };
 
+//switch colors
+const colorsLi = document.querySelectorAll(".colors-list li");
+colorsLi.forEach((li) => {
+  li.addEventListener("click", (e) => {
+    document.documentElement.style.setProperty(
+      "--main--color",
+      e.target.dataset.color
+    );
+  });
+});
+
 let land = document.querySelector(".landing-page");
 let imgArray = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
 setInterval(function () {
